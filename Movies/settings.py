8ee92b671924cd3c2c1041a4f9e0c8a50c115d57
Sully <salmaamoohamedsm@gmail.com>
2025-08-22 05:57:48 +0300

    add installed apps to settings

--- Movies/settings.py
+++ Movies/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'Reviews',
+    'Accounts',
+    'Movies',  # Assuming 'Movies' is the main app for the project
+    'django_filters',  # For filtering functionality
 
 ]
 
