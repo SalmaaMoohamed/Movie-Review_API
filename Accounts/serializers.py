eb9cac8abcedaa99139668c4ee454d45aa856040
Sully <salmaamoohamedsm@gmail.com>
2025-08-22 06:03:04 +0300

    Add serializers.py

---
+++ Accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username', 'email', 'profile_picture', 'bio')  
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            profile_picture=validated_data.get('profile_picture'),
+            bio=validated_data.get('bio')
+        )
+        Token.objects.create(user=user)
+        return user
+    
