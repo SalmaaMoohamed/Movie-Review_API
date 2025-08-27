5945d06f46cd06525885142bbe1fcd3e29502aef
Sully <salmaamoohamedsm@gmail.com>
2025-08-27 07:07:08 +0300

    Update views.py

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,94 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django import forms
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+# accounts/views.py
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:profile')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.GET.get('next')
+                return redirect(next_url or reverse('accounts:profile'))
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:profile')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            
+            login(request, user)
+            return redirect('accounts:profile')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html', {'user': request.user})
+
+
+class SimpleProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+@login_required
+def edit_profile_view(request):
+    user = request.user
+    if request.method == 'POST':
+        form = SimpleProfileForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:profile')
+    else:
+        form = SimpleProfileForm(instance=user)
+
+    return render(request, 'accounts/edit_profile.html', {'form': form})
+
+
+@login_required
+def password_change_view(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            
+            from django.contrib.auth import update_session_auth_hash
+            update_session_auth_hash(request, form.user)
+            return redirect('accounts:profile')
+    else:
+        form = PasswordChangeForm(user=request.user)
+
+    return render(request, 'accounts/password_change.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
