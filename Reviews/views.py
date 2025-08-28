55b864f452226c73234f0ef116faef58ae9da857
Sully <salmaamoohamedsm@gmail.com>
2025-08-28 06:03:00 +0300

    Update views.py

--- Reviews/views.py
+++ Reviews/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from .models import Review
-
-# Create your views here.
-# reviews/views.py
-
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Review, Movie
-from .forms import ReviewForm  
+from .models import Movie
+from .forms import ReviewForm
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from .models import Review
+from .serializers import ReviewSerializer
+
+
+# Create your views here.
+# reviews/views.py
 
 class ReviewListView(ListView):
     model = Review
@@ -78,3 +82,31 @@ class ReviewUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("reviews:detail", kwargs={"pk": self.object.pk})
+    
+
+
+# API ViewSet for Reviews 
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    # مثال إضافي: تخصيص create ليظهر 201 مباشرة وبالتأكد من validity
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    # مثال إضافي: تخصيص retrieve ليظهر 404 عند غير موجود
+    def retrieve(self, request, pk=None):
+        obj = get_object_or_404(Review, pk=pk)
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
+
+    # مثال إضافي: تخصيص destroy ليعيد 204 على النجاح
+    def destroy(self, request, pk=None):
+        obj = get_object_or_404(Review, pk=pk)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
