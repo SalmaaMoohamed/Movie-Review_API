8f01773b9918af615c6484bf07e97b3e90596ab2
Sully <salmaamoohamedsm@gmail.com>
2025-08-31 17:42:34 +0300

    Update models.py

--- Reviews/models.py
+++ Reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from .models import User
+
 from django.contrib.auth.models import User
 from .models import Movie
 from .models import Review
@@ -10,19 +10,7 @@ from .models import Genre
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
-        ordering = ["username"]
 
-    def __str__(self):
-        return self.username
 
 
 
