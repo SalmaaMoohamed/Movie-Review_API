83f1cf1df794bf7a3b6e1b23c74ced754492d51e
Sully <salmaamoohamedsm@gmail.com>
2025-09-06 00:17:45 +0300

    Update models.py

--- Accounts/models.py
+++ Accounts/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=15, unique=True)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=10)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
 
